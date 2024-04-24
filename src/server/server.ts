dbd340f95dc431ac2ff9536eb7b468a9b3697c7b
patrykjachym <jachympatryk99@gmail.com>
2024-04-24 18:45:32 +0200

    update env

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,7 +15,8 @@ export async function fetchFromBackend<T = never>(
   endpoint: string,
   { method = 'GET', body, headers = {} }: FetchOptions = {}
 ): Promise<T> {
-  const apiKey = process.env.REACT_APP_API_KEY as string;
+  const apiKey = import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY as string;
+
   const config: RequestInit = {
     method,
     headers: {
@@ -24,6 +25,8 @@ export async function fetchFromBackend<T = never>(
       ...headers,
     },
   };
+
+  console.log(config.headers);
 
   if (body) {
     config.body = JSON.stringify(body);
